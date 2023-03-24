f85e5a70912e41fc6f16703f529b251fe3220676
theo <theo.oliver@epita.fr>
2023-03-24 12:20:20 +0700

    fina

---
+++ Part 6 - AppThemes/MonkeyFinder/Model/Monkey.cs
@@ -0,0 +1,9 @@
+﻿namespace MonkeyFinder.Model;
+
+public class Monkey
+{
+    public string craft { get; set; }
+    public string name { get; set; }
+    public string image { get; set; }
+    public string details { get; set; }
+}
---
+++ Part 6 - AppThemes/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace MonkeyFinder.ViewModel;
+
+[QueryProperty(nameof(Monkey), "Monkey")]
+public partial class MonkeyDetailsViewModel : BaseViewModel
+{
+    IMap map;
+    public MonkeyDetailsViewModel(IMap map)
+    {
+        this.map = map;
+    }
+
+    [ObservableProperty]
+    Monkey monkey;
+
+    [RelayCommand]
+    async Task OpenMap()
+    {
+    }
+}
